567cd699119a51f1bb4a64824f2f804c0213e7a6
Manvi Rankawat <81751478+ManviRankawat@users.noreply.github.com>
2024-10-13 22:00:47 -0400

    Time: 5 ms (73.2%), Space: 43.9 MB (63.83%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,30 +1,16 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        Stack<Integer> s = new Stack<>();
-
-        if(x < 0){
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
         }
-        
-        if(0<=x && x<10){
-            return true;
-        }
-        int original = x;
 
-        while(x != 0){
-            s.push(x%10);
-            x/=10;
+        int reversedHalf = 0;
+
+        while (x > reversedHalf) {
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x /= 10;
         }
 
-        int fact = 1;
-        int reversed = 0;
-
-        while(!s.isEmpty()){
-            reversed += s.pop() * fact;
-            fact *= 10;
-        }
-
-        return reversed == original;
-        
+        return x == reversedHalf || x == reversedHalf / 10;
     }
 }
