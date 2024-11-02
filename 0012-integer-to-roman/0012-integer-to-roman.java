0cbdb9a2921ead485e19ffdc8e2a63d300ad52dd
Manvi Rankawat <81751478+ManviRankawat@users.noreply.github.com>
2024-11-02 14:51:43 -0400

    Time: 3 ms (98.68%), Space: 44.3 MB (52.8%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,13 +1,21 @@
 class Solution {
     public String intToRoman(int num) {
-        String[] thousands = {"", "M", "MM", "MMM"};
-        String[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
-        String[] tens = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
-        String[] ones = {"","I","II","III","IV","V","VI","VII", "VIII","IX"};
-
-        return thousands[num/1000]+
-        hundreds[(num%1000)/100]+
-        tens[(num%100)/10]+
-        ones[num%10];
+        // Define mappings of integer values to Roman numeral symbols
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        
+        StringBuilder roman = new StringBuilder();
+        
+        // Loop through each value
+        for (int i = 0; i < values.length && num > 0; i++) {
+            // Determine the number of times the current value fits into num
+            while (num >= values[i]) {
+                // Append the corresponding symbol and reduce num
+                roman.append(symbols[i]);
+                num -= values[i];
+            }
+        }
+        
+        return roman.toString();
     }
 }
