55e270135fa7e564cd6d59bf92098c74ba22d242
Manvi Rankawat <81751478+ManviRankawat@users.noreply.github.com>
2025-07-15 16:35:19 -0400

    Time: 1 ms (47.89%), Space: 42 MB (27.16%) - LeetHub

--- 0068-text-justification/0068-text-justification.java
+++ 0068-text-justification/0068-text-justification.java
@@ -1,36 +1,46 @@
-class Solution {
-    public List<String> fullJustify(String[] words, int maxWidth) {
-        List<String> result = new ArrayList<>();
-        int i = 0;
-        while (i < words.length) {
-            int j = i, len = 0;
-            while (j < words.length && len + words[j].length() + (j - i) <= maxWidth) {
-                len += words[j].length();
-                j++;
+public class Solution {
+    public List<String> fullJustify(String[] words, int L) {
+        List<String> result = new ArrayList<String>();
+        
+        int index = 0;
+        while (index < words.length) {
+            int count = words[index].length();
+            int last = index + 1;
+            while (last < words.length) {
+                if (words[last].length() + count + 1 > L) break;
+                count += words[last].length() + 1;
+                last++;
             }
-            int gaps = j - i - 1;
-            int spaces = maxWidth - len;
-            StringBuilder line = new StringBuilder();
-
-            if (j == words.length || gaps == 0) {
-                for (int k = i; k < j; k++) {
-                    line.append(words[k]);
-                    if (k != j - 1) line.append(" ");
+            
+            StringBuilder builder = new StringBuilder();
+            int diff = last - index - 1;
+            // if last line or number of words in the line is 1, left-justified
+            if (last == words.length || diff == 0) {
+                for (int i = index; i < last; i++) {
+                    builder.append(words[i] + " ");
                 }
-                while (line.length() < maxWidth) line.append(" ");
+                builder.deleteCharAt(builder.length() - 1);
+                for (int i = builder.length(); i < L; i++) {
+                    builder.append(" ");
+                }
             } else {
-                int spaceEach = spaces / gaps, extra = spaces % gaps;
-                for (int k = i; k < j; k++) {
-                    line.append(words[k]);
-                    if (k != j - 1) {
-                        int toAdd = spaceEach + (extra-- > 0 ? 1 : 0);
-                        line.append(" ".repeat(toAdd));
+                // middle justified
+                int spaces = (L - count) / diff;
+                int r = (L - count) % diff;
+                for (int i = index; i < last; i++) {
+                    builder.append(words[i]);
+                    if (i < last - 1) {
+                        for (int j = 0; j <= (spaces + ((i - index) < r ? 1 : 0)); j++) {
+                            builder.append(" ");
+                        }
                     }
                 }
             }
-            result.add(line.toString());
-            i = j;
+            result.add(builder.toString());
+            index = last;
         }
+        
+        
         return result;
     }
 }
