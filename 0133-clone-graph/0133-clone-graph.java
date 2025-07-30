57a0836bb3636cf7b8080a511023b0c6e91d58ae
Manvi Rankawat <81751478+ManviRankawat@users.noreply.github.com>
2025-07-30 18:37:39 -0400

    Time: 30 ms (6.99%), Space: 43 MB (35.18%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.java
@@ -0,0 +1,46 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+    if (node == null) return null;
+    
+    Map<Node, Node> visited = new HashMap<>();
+    Queue<Node> queue = new LinkedList<>();
+    
+    Node clone = new Node(node.val);
+    visited.put(node, clone);
+    queue.offer(node);
+    
+    while (!queue.isEmpty()) {
+        Node current = queue.poll();
+        
+        for (Node neighbor : current.neighbors) {
+            if (!visited.containsKey(neighbor)) {
+                visited.put(neighbor, new Node(neighbor.val));
+                queue.offer(neighbor);
+            }
+            visited.get(current).neighbors.add(visited.get(neighbor));
+        }
+    }
+    
+    return clone;
+}
+}
