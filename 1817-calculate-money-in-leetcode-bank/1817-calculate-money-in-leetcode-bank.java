6f560db6c0c136bb2cbe3e921edf31b2bf0b6709
Manvi Rankawat <81751478+ManviRankawat@users.noreply.github.com>
2025-10-24 23:03:06 -0400

    Time: 1 ms (56.85%), Space: 40.3 MB (76.33%) - LeetHub

--- 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.java
+++ 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.java
@@ -1,14 +1,12 @@
 class Solution {
     public int totalMoney(int n) {
-        int arr[] = new int[n];
+        int mondayCal = 1;
         int sum = 0;
         for(int i = 0; i < n; i ++){
-            if(i % 7 == 0){
-                arr[i] = (i/7) + 1;
-            }else{
-                arr[i] = arr[i - 1] + 1;
+            if(i % 7 == 0 && i > 0){
+                mondayCal++;
             }
-            sum += arr[i];
+            sum += mondayCal + (i % 7);
         }
         return sum;
     }
