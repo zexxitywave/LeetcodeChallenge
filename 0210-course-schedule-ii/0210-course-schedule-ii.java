397add3d981e671ac18a01e26eed2bb668bdfe10
Manvi Rankawat <81751478+ManviRankawat@users.noreply.github.com>
2025-11-11 18:48:33 -0500

    Time: 5 ms (79.2%), Space: 46.8 MB (12.65%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> graph = new ArrayList<>();
+        int[] indegree = new int[numCourses];
+        
+        for (int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<>());
+        }
+        
+        for (int[] prereq : prerequisites) {
+            int course = prereq[0];
+            int prerequisite = prereq[1];
+            graph.get(prerequisite).add(course);
+            indegree[course]++;
+        }
+        
+        // Add all courses with no prerequisites to queue
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        
+        // Process courses in topological order
+        int[] result = new int[numCourses];
+        int index = 0;
+        
+        while (!queue.isEmpty()) {
+            int course = queue.poll();
+            result[index++] = course;
+            
+            for (int next : graph.get(course)) {
+                indegree[next]--;
+                if (indegree[next] == 0) {
+                    queue.offer(next);
+                }
+            }
+        }
+
+        return index == numCourses ? result : new int[0];
+    }
+}
