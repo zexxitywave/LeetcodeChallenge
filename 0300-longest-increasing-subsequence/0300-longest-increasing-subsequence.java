5da4b3970c0bc7e7fe573423fd6903c149bbdcb2
Manvi Rankawat <81751478+ManviRankawat@users.noreply.github.com>
2025-11-18 09:36:31 -0500

    Time: 49 ms (34.41%), Space: 46.1 MB (35.55%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,23 +1,23 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[] seqDP = new int[nums.length];
+        int incSeq[] = new int[nums.length];
 
         for(int i = 1; i < nums.length; i++){
             for(int j = 0; j < i; j++){
+
                 if(nums[i] > nums[j]){
-                    if(seqDP[j]+1 > seqDP[i]){
-                        seqDP[i] = seqDP[j] + 1;
-                    }
+                    incSeq[i] = Math.max(incSeq[j] + 1, incSeq[i]);
                 }
             }
         }
 
-        int maxIndex = 0;
-        for(int i = 0; i < seqDP.length; i++){
-            if(seqDP[i] > seqDP[maxIndex]){
-                maxIndex = i;
+        int maxSeq = 0;
+        for(int i = 1; i < incSeq.length; i++){
+            if(incSeq[i] > incSeq[maxSeq]){
+                maxSeq = i;
             }
         }
-        return seqDP[maxIndex] + 1;
+
+        return incSeq[maxSeq] + 1;
     }
 }
