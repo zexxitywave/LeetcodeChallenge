de8258b2c610ac1b934ad3fd3487b0dae970cfa4
Manvi Rankawat <81751478+ManviRankawat@users.noreply.github.com>
2025-11-20 14:18:07 -0500

    Time: 1 ms (99.34%), Space: 42.7 MB (76.65%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -3,22 +3,23 @@ class Solution {
         Set<String> wordSet = new HashSet<>(wordDict);
 
         int maxLen = 0;
-        for(String word : wordDict){
-            maxLen = Math.max(maxLen, word.length());
+        for(String i : wordDict){
+            maxLen = Math.max(i.length(), maxLen);
         }
 
-        boolean[] dp = new boolean[s.length() + 1];
+        int n = s.length();
 
+        boolean dp[] = new boolean[n+1];
         dp[0] = true;
 
-        for(int i = 1; i <= s.length(); i++){
-            for(int j = i-1; j >= Math.max(0, i - maxLen); j--){
-                if(dp[j] && wordSet.contains(s.substring(j,i))){
+        for(int i = 1; i <= n; i++){
+            for(int j = i - 1; j >= Math.max(0, i - maxLen); j--){
+                if(dp[j] && wordSet.contains(s.substring(j, i))){
                     dp[i] = true;
                     break;
                 }
             }
         }
-        return dp[s.length()];
+        return dp[n];
     }
 }
