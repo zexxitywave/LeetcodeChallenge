ae11f73fc52cfe9c9dd38b1c8c397c427f36eeee
Manvi Rankawat <81751478+ManviRankawat@users.noreply.github.com>
2025-12-07 19:58:40 -0500

    Time: 13 ms (56.16%), Space: 42.2 MB (37.58%) - LeetHub

---
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countTriples(int n) {
+        int count = 0;
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                int sum = i*i + j*j;
+                int k = (int)Math.sqrt(sum);
+                if(k*k == sum && k <= n){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
