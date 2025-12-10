5910e3a691b4ec7bcdfd575dd71694fbde15a46b
Manvi Rankawat <81751478+ManviRankawat@users.noreply.github.com>
2025-12-10 18:07:25 -0500

    Time: 1 ms (89.44%), Space: 43.3 MB (17.63%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder merge = new StringBuilder();
+        int maxLen = Math.max(word1.length(), word2.length());
+
+        for(int i = 0; i < maxLen; i++){
+            if(i < word1.length()){
+                merge.append(word1.charAt(i));
+            }
+            if(i < word2.length()){
+                merge.append(word2.charAt(i));
+            }
+        }
+
+        return merge.toString();
+    }
+}
